b36c0ea40a55233d8ec35cb6638a333e9cd0fb4d
amol <amol.deshpande@outlook.com>
2020-12-28 17:38:11 -0800

    another attempt at improving utf8 support. doesn't look like it's possible without rewriting a bunch of display code.

--- win32/nt.char.c
+++ win32/nt.char.c
@@ -44,6 +44,7 @@ static HMODULE hlangdll;
 extern DWORD gdwPlatform;
 
 
+#if !defined(WINNT_NATIVE_UTF8_SUPPORT)
 unsigned char oem_it(unsigned char ch) {
 	unsigned char ch1[2],ch2[2];
 
@@ -54,6 +55,11 @@ unsigned char oem_it(unsigned char ch) {
 
 	return ch2[0];
 }
+#else
+unsigned char oem_it(unsigned char ch) {
+	return ch;
+}
+#endif
 void nls_dll_unload(void) {
 	FreeLibrary(hlangdll);
 	hlangdll=NULL;
@@ -124,3 +130,91 @@ void nls_dll_init(void) {
 		free(ptr);
 	}
 }
+int NLSWidth(Char c)
+{
+	WCHAR out;
+	int result = MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS,(char*)&c,1,&out, 1);
+	return (result > 0 ? result : -1);
+}
+int NLSWidthMB(Char* cp, int*consumed)
+{
+	int result = 0;
+	WCHAR out;
+	char four[4];
+	int len = 4;
+
+	*consumed = 0;
+
+	for(int i = 0; i < 4 ;i++) {
+		four[i] = *cp & CHAR;
+		if(!*cp) {
+			len = i ;
+			break;
+		}
+		cp++;
+	}
+	for(int i = 0; i < len; i++) {
+		result = MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS,four,i+1,&out, 0);
+		if(result > 0) {
+			*consumed = i+1;
+			break;
+		}
+	}
+	dprintf("NSLWidthMB returning %d consumed\n",*consumed);
+	return result;
+}
+int
+NLSStringWidth(const Char *s)
+{
+	WCHAR out;
+	int result = MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS,(char*)s,-1,&out, 0);
+
+	return result;
+}
+int NLSClassifyMB(Char* cp, int nocomb, int drawPrompt,int* consumed)
+{
+	int w;
+	Char c = *cp & CHAR;
+	*consumed = 0;
+
+#ifndef SHORT_STRINGS
+	if ((c & 0x80) != 0)		/* c >= 0x80 */
+		return NLSCLASS_ILLEGAL;
+#endif
+	if (!drawPrompt) {			/* draw command-line */
+#if INVALID_BYTE != 0
+		if ((c & INVALID_BYTE) == INVALID_BYTE)		/* c >= INVALID_BYTE */
+			return NLSCLASS_ILLEGAL;
+		if ((c & INVALID_BYTE) == QUOTE && (c & 0x80) == 0)	/* c >= QUOTE */
+			return 1;
+		if (c >= 0x10000000)		/* U+10000000 = FC 90 80 80 80 80 */
+			return NLSCLASS_ILLEGAL5;
+		if (c >= 0x1000000)		/*  U+1000000 = F9 80 80 80 80 */
+			return NLSCLASS_ILLEGAL4;
+		if (c >= 0x100000)		/*   U+100000 = F4 80 80 80 */
+			return NLSCLASS_ILLEGAL3;
+#endif
+		if (c >= 0x10000)		/*    U+10000 = F0 90 80 80 */
+			return NLSCLASS_ILLEGAL2;
+	}
+#pragma warning(suppress:4310) //cast truncates constant value
+	if (Iscntrl(c) && (c & CHAR) < 0x100) {
+		if (c == '\n')
+			return NLSCLASS_NL;
+		if (c == '\t')
+			return NLSCLASS_TAB;
+		return NLSCLASS_CTRL;
+	}
+	w = NLSWidthMB(cp,consumed);
+	if (drawPrompt) {			/* draw prompt */
+		if (w > 0)
+			return w;
+		if (w == 0)
+			return 1;
+	}
+#pragma warning(suppress:4310) //cast truncates constant value
+	if ((w > 0 && !(Iscntrl(c) && (c & CHAR) < 0x100)) || (Isprint(c) && !nocomb))
+		return w;
+	return NLSCLASS_ILLEGAL;
+}
+
