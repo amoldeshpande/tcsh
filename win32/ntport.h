4b8dc97df15e8f5886da63fd8b2f6b7a8b71fb20
amol <amol.deshpande@outlook.com>
2021-02-11 17:01:46 -0800

    Clean up code around Function and other key bindings NT_NUM_KEYS is back to 256 for the full vt case, support conditionally compiling it that way

--- win32/ntport.h
+++ win32/ntport.h
@@ -197,6 +197,7 @@ mode Value	Checks File For
 #define R_OK 4
 #define XD_OK 9 //executable and not directory
 
+#ifdef WINNT_NATIVE_NO_UTF8
 /* base key mappings + ctrl-key mappings + alt-key mappings */
 /* see nt.bind.c  to figure these out */
 /*  256 + 
@@ -218,6 +219,7 @@ mode Value	Checks File For
 #define CTRL_KEY_OFFSET            34
 #define ALT_KEY_OFFSET             (34*2)
 #define SHIFT_KEY_OFFSET           (34*3)
+#endif
 
 typedef int pid_t;
 typedef int speed_t;
